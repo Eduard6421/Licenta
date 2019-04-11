3468812b5319c031bcfd0b9719d1929b0fb6ba73
Eduard6421 <eduard_poe2000@yahoo.com>
2019-04-12 01:48:44 +0300

    Rearranged the entire script library

---
+++ Assets/Scripts/Builders/BehaviourBuilder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BehaviourBuilder
+{
+    public abstract SteeringOutput getAvoidanceGroupSteering();
+    public abstract SteeringOutput getMovementGroupSteering();
+}
